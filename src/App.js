bfc21645c6defaebcdf36806b8ca5ec366b5f5c7
santhosh s <subramanisanthosh00@gmail.com>
2023-02-25 19:32:20 +0530

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function checkNum(j) {
+  return (j % 2 === 0);
+}
+
+function prime(n) {
+  if(n>1){
+    for(let l=2;l<=n-1;l++){
+      if(n%l === 0){
+        return false
+      }
+    }
+    return true
+  }
+}
+
+const yellow = {
+  background: "#FDDB3A",
+};
+const green = {
+  background: "#21BF73",
+};
+const red = {
+  background: "#FD5E53",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="heading">30 Days Of React</div>
+      <h2 className="h2">Number Generator</h2>
+      <div className="Container">
+        {Array(32)
+          .fill("")
+          .map((e, i) => {
+            return (
+              <div className="card" style={checkNum(i)?prime(i)? red: green :prime(i)? red: yellow}>
+                <h1>{i}</h1>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
